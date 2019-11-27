f381f853ec4723622fd063deb8b9f65a35d494bd
张健夫 <451458765@qq.com>
2019-11-27 17:42:06 +0800

    方法修改

--- lib/flutter_audio_manager_plugin.dart
+++ lib/flutter_audio_manager_plugin.dart
@@ -6,7 +6,7 @@ class FlutterAudioManagerPlugin {
   static const MethodChannel _channel =
       const MethodChannel('flutter_audio_manager_plugin');
 
-  static Future<int> get requestAudioFocus async {
+  static Future<int> requestAudioFocus() {
     return _channel.invokeMethod('requestAudioFocus');
   }
 }
